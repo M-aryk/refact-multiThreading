8751b1b94ae2d0969b5b1a84c3a718eec134dac5
Marina Malysheva <yumar1420@gmail.com>
2024-12-03 20:28:42 +0300

    add connectionProcessing()

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ public class Server {
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
             "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html",
             "/classic.html", "/events.html", "/events.js");
+    private Socket socket;
 
     public Server(int port, int threadPool) {
         this.port = port;
@@ -26,23 +28,8 @@ public class Server {
         try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
                 try {
-                    final var socket = serverSocket.accept();
-                    executorService.execute(() -> {
-                        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                             final var out = new BufferedOutputStream(socket.getOutputStream())) {
-                            final var requestLine = in.readLine();
-                            final var parts = requestLine.split(" ");
-                            handling(parts, out);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                socket.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+                    socket = serverSocket.accept();
+                    connectionProcessing();
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -51,6 +38,25 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void connectionProcessing(){
+        executorService.execute(() -> {
+            try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 final var out = new BufferedOutputStream(socket.getOutputStream())) {
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
+                handling(parts, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void handling(String[] parts, BufferedOutputStream out) {
